0844b6b9834d86219623c448597079504a29870f
Muhammad Kashif Zia <usama.idenbrid@gmail.com>
2022-09-20 11:27:06 +0500

    push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,18 +8,18 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index(){
-    if(Auth::user()->hasrole('story')){
+    if(Auth::user()->hasrole('story') && Auth::user()->is_active == '1'){
         return view('story.dashboard');
     }
-    elseif(Auth::user()->hasrole('reader')){
+    elseif(Auth::user()->hasrole('reader')  && Auth::user()->is_active == '1'){
 
         return view('reader.dashboard');
     }
-    elseif(Auth::user()->hasrole('admin')){
+    elseif(Auth::user()->hasrole('admin')  && Auth::user()->is_active == '1'){
 
         return view('admin.dashboard');
     }
-    elseif(Auth::user()->hasrole('user')){
+    elseif(Auth::user()->hasrole('user')  && Auth::user()->is_active == '1'){
 
         return view('user.dashboard');
     }
