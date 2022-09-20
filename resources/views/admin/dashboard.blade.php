91c7ffd613a28147b3072ea3fdfe3c12da3eb5cb
HamzaIB01 <109939718+HamzaIB01@users.noreply.github.com>
2022-09-20 15:08:57 +0500

    Add New Reader Page Completed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,6 +5,11 @@
     </h2>
   </x-slot>
   <section>
-   <h1>Dahsboard</h1>
+    <!-- Page Heading -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-0">
+      <h2 class="bg-white text-sky-500 text-4xl">
+        Dashboard
+      </h2>
+    </div>
   </section>
 </x-app-layout>
