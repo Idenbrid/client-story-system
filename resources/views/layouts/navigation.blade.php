44aee1b16e029aa1ea130e1ced3bbf540be507ac
sammad-idenbrid <96107106+sammad-idenbrid@users.noreply.github.com>
2022-09-29 09:55:53 +0500

    navigation fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -63,7 +63,7 @@
                     </x-nav-link>
                 </div>
                 @endif
-                </div>
+
         {{-- END of Navigation Links FOR SourceAdmin  --}}
         {{-- @endif --}}
         <!-- Settings Dropdown -->
@@ -112,7 +112,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            
+
 
                 {{-- Start of Mobile Navigation for Admin --}}
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -128,8 +128,8 @@
                     {{ __('Luca Users') }}
                 </x-responsive-nav-link>
                 {{-- End of Mobile Navigation for Admin --}}
-            
-                
+
+
                 {{-- Start of Mobile Navigation for SourceAdmin --}}
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
@@ -144,7 +144,7 @@
                     {{ __('Luca Users') }}
                 </x-responsive-nav-link>
                 {{-- End of Mobile Navigation for SourceAdmin --}}
-            
+
         </div>
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
